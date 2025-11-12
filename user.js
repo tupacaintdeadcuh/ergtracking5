8fa2a3538eae388c4d9c6294322a77615e1fa395
tupacaintdeadcuh <sloanb123@icloud.com>
2025-11-11 21:20:21 -0600

    Delete user.js

--- user.js
+++
@@ -1,7 +0,0 @@
-import { getCookie, verify } from './_util.js';
-export default async function handler(req, res){
-  const token = getCookie(req);
-  const payload = verify(token, process.env.SESSION_SECRET);
-  if (!payload) return res.status(200).json({ loggedIn:false });
-  res.status(200).json({ loggedIn:true, user: payload });
-}
