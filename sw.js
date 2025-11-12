dac8015d72e6838c36c0c8e7883b84499609bfa8
tupacaintdeadcuh <sloanb123@icloud.com>
2025-11-11 21:20:07 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('erg-v1').then(c=>c.addAll(['./index.html','./styles.css','./app.js','./admin.html','./manifest.webmanifest'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
